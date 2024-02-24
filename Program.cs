337fa6d58226a7726d9dab9d5cf74dc42812f50e
tilak-giri <tilakgiri01@gmail.com>
2024-02-24 17:15:32 +0545

    trying fizzbuzz challange

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿int number;
+for (int i=1; i<=100; i++)
+{
+    number = i;
+    if(number%3 == 0){
+        Console.WriteLine($"{number} - Fizz");
+        if (number%5 == 0){
+            Console.WriteLine($"{number} - FizzBuzz");
+        }
+    }
+    else if(number%5 == 0){
+        Console.WriteLine($"{number} - Buzz");
+    }
+    else{
+        Console.WriteLine(number);
+    }
+}
